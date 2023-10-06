a804e9d32a47415a6a56ed4f843feb59290d816b
lamiselzobeidy <lamis.zobeidy@gmail.com>
2023-10-06 22:32:00 +0300

    Added routes for the system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { EmployeesPageComponent } from './components/employees-page/employees-page.component';
+import { DepartmentsPageComponent } from './components/departments-page/departments-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listing/employees', component: EmployeesPageComponent },
+  { path: 'listing/departments', component: DepartmentsPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
