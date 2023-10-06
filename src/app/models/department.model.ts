a00e9f6fbca916e4a706ee821f37e9cd76b7aec7
lamiselzobeidy <lamis.zobeidy@gmail.com>
2023-10-06 22:25:05 +0300

    Created employees and departments models

---
+++ src/app/models/department.model.ts
@@ -0,0 +1,6 @@
+export interface Department {
+  id?: number;
+  name: string;
+  order: number;
+  Active: boolean;
+}
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id?: number;
+  name: string;
+  identification: string;
+  phone: string;
+  mobile: string;
+  Active: boolean;
+  hireDate: Date;
+  department: string;
+  position: string;
+}
